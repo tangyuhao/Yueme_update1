80ad6636b7957729cd14e1d902227ac87a42c59f
yjw <wsklxsx@126.com>
2015-02-26 22:14:32 +0800

    add missed file
    Signed-off-by: yjw <wsklxsx@126.com>

--- app/src/main/java/com/syc/yueme/ui/fragment/DiscoverFragment.java
+++ app/src/main/java/com/syc/yueme/ui/fragment/DiscoverFragment.java
@@ -1,23 +1,40 @@
 package com.syc.yueme.ui.fragment;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.AVUser;
 import com.syc.yueme.R;
+import com.syc.yueme.avobject.ChatGroup;
+import com.syc.yueme.avobject.Message;
+import com.syc.yueme.service.ChatService;
+import com.syc.yueme.service.GroupService;
+import com.syc.yueme.service.MessageService;
 import com.syc.yueme.service.UserService;
+
 import butterknife.InjectView;
+
 import com.syc.yueme.adapter.NearPeopleAdapter;
 import com.syc.yueme.service.PreferenceMap;
 import com.syc.yueme.ui.activity.PersonInfoActivity;
 import com.syc.yueme.ui.activity.YueUpdateActivity;
 import com.syc.yueme.ui.view.BaseListView;
 import com.nostra13.universalimageloader.core.listener.PauseOnScrollListener;
+
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
 import com.syc.yueme.util.Utils;
+import com.syc.yueme.avobject.Message;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,28 +44,31 @@ import java.util.List;
 public class DiscoverFragment extends BaseFragment implements View.OnClickListener {
 
     Button yueButton;
-  @InjectView(R.id.list_near)
-  BaseListView<AVUser> listView;
+    @InjectView(R.id.list_near)
+    BaseListView<AVObject> listView;
 
-  NearPeopleAdapter adapter;
-  List<AVUser> nears = new ArrayList<AVUser>();
-  int orderType;
-  PreferenceMap preferenceMap;
+    NearPeopleAdapter adapter;
+    List<AVObject> nears = new ArrayList<AVObject>();
+    public static List<List<AVObject>> coms;
+    int orderType;
+    public static RelativeLayout commentLayout;
+    public static EditText commentEdit;
+    public static Button commentBtn;
+    PreferenceMap preferenceMap;
 
- private final SortDialogListener distanceListener = new SortDialogListener(UserService.ORDER_DISTANCE);
-  private final SortDialogListener updatedAtListener = new SortDialogListener(UserService.ORDER_UPDATED_AT);
+    private final SortDialogListener distanceListener = new SortDialogListener(UserService.ORDER_DISTANCE);
+    private final SortDialogListener updatedAtListener = new SortDialogListener(UserService.ORDER_UPDATED_AT);
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.discover_fragment, container, false);
-  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.discover_fragment, container, false);
+    }
 
     private void findView() {
         View fragmentView = getView();
         yueButton = (Button) fragmentView.findViewById(R.id.btn_yue);
-       yueButton.setOnClickListener(this);
+        yueButton.setOnClickListener(this);
     }
-
 
 
     @Override
@@ -56,69 +76,68 @@ public class DiscoverFragment extends BaseFragment implements View.OnClickListen
         int id = v.getId();
         if (id == R.id.btn_yue) {
             Utils.goActivity(ctx, YueUpdateActivity.class);
+
         }
     }
 
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    preferenceMap = PreferenceMap.getCurUserPrefDao(getActivity());
-    orderType = preferenceMap.getNearbyOrder();
-    headerLayout.showTitle(R.string.discover);
-    headerLayout.showRightImageButton(R.drawable.nearby_order, new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.sort).setPositiveButton(R.string.loginTime,
-            updatedAtListener).setNegativeButton(R.string.distance, distanceListener).show();
-      }
-    });
-    initXListView();
-    listView.onRefresh();
-      findView();
-  }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        commentLayout = (RelativeLayout) getView().findViewById(R.id.comment_layout);
+        commentEdit = (EditText) getView().findViewById(R.id.comment_text_edit);
+        commentBtn = (Button) getView().findViewById(R.id.commentBtn);
+        headerLayout.showTitle(R.string.discover);
+        headerLayout.showRightImageButton(R.drawable.nearby_order, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.sort).setPositiveButton(R.string.loginTime,
+                        updatedAtListener).setNegativeButton(R.string.distance, distanceListener).show();
+            }
+        });
+        initXListView();
+        listView.onRefresh();
+        findView();
+    }
 
 
+    public class SortDialogListener implements DialogInterface.OnClickListener {
+        int orderType;
 
-  public class SortDialogListener implements DialogInterface.OnClickListener {
-    int orderType;
+        public SortDialogListener(int orderType) {
+            this.orderType = orderType;
+        }
 
-    public SortDialogListener(int orderType) {
-      this.orderType = orderType;
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            listView.onRefresh();
+        }
+    }
+
+
+    private void initXListView() {
+        adapter = new NearPeopleAdapter(ctx, nears);
+        listView = (BaseListView<AVObject>) getView().findViewById(R.id.list_near);
+        coms = new ArrayList<List<AVObject>>();
+        listView.init(new BaseListView.DataInterface<AVObject>() {
+            @Override
+            public List<AVObject> getDatas(int skip, int limit, List<AVObject> currentDatas) throws Exception {
+                List<AVObject> msgs = MessageService.findMsg(skip, limit);
+                return msgs;
+            }
+
+            @Override
+            public void onItemSelected(AVObject item) {
+            }
+        }, adapter);
+        listView.onRefresh();
+//        PauseOnScrollListener listener = new PauseOnScrollListener(MessageService.imageLoader,
+//                true, true);
+//        listView.setOnScrollListener(listener);
     }
 
     @Override
-    public void onClick(DialogInterface dialog, int which) {
-      DiscoverFragment.this.orderType = orderType;
-      listView.onRefresh();
+    public void onDestroy() {
+        super.onDestroy();
     }
-  }
-
-
-
-  private void initXListView() {
-    adapter = new NearPeopleAdapter(ctx, nears);
-    listView = (BaseListView<AVUser>) getView().findViewById(R.id.list_near);
-    listView.init(new BaseListView.DataInterface<AVUser>() {
-      @Override
-      public List<AVUser> getDatas(int skip, int limit, List<AVUser> currentDatas) throws Exception {
-        return UserService.findNearbyPeople(orderType, skip, limit);
-      }
-
-      @Override
-      public void onItemSelected(AVUser item) {
-        PersonInfoActivity.goPersonInfo(ctx, item.getObjectId());
-      }
-    }, adapter);
-
-    PauseOnScrollListener listener = new PauseOnScrollListener(UserService.imageLoader,
-        true, true);
-    listView.setOnScrollListener(listener);
-  }
-
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    preferenceMap.setNearbyOrder(orderType);
-  }
 }
