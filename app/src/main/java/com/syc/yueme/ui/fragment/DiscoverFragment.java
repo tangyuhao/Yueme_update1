149584ba8ca7bfd50c1f23f519d20075fcbe2688
tangyuhao <yhtang1993@126.com>
2015-02-28 00:22:39 +0800

    修改联系人右上角button

--- app/src/main/java/com/syc/yueme/ui/fragment/DiscoverFragment.java
+++ app/src/main/java/com/syc/yueme/ui/fragment/DiscoverFragment.java
@@ -13,6 +13,7 @@ import com.avos.avoscloud.AVUser;
 import com.syc.yueme.R;
 import com.syc.yueme.avobject.ChatGroup;
 import com.syc.yueme.avobject.Message;
+import com.syc.yueme.base.App;
 import com.syc.yueme.service.ChatService;
 import com.syc.yueme.service.GroupService;
 import com.syc.yueme.service.MessageService;
@@ -22,6 +23,7 @@ import butterknife.InjectView;
 
 import com.syc.yueme.adapter.NearPeopleAdapter;
 import com.syc.yueme.service.PreferenceMap;
+import com.syc.yueme.ui.activity.AddFriendActivity;
 import com.syc.yueme.ui.activity.PersonInfoActivity;
 import com.syc.yueme.ui.activity.YueUpdateActivity;
 import com.syc.yueme.ui.view.BaseListView;
@@ -32,6 +34,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.syc.yueme.ui.view.HeaderLayout;
 import com.syc.yueme.util.Utils;
 import com.syc.yueme.avobject.Message;
 
@@ -41,8 +44,8 @@ import java.util.List;
 /**
  * Created by lzw on 14-9-17.
  */
-public class DiscoverFragment extends BaseFragment implements View.OnClickListener {
-
+public class DiscoverFragment extends BaseFragment {
+//    implements View.OnClickListener
     Button yueButton;
     @InjectView(R.id.list_near)
     BaseListView<AVObject> listView;
@@ -63,22 +66,41 @@ public class DiscoverFragment extends BaseFragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.discover_fragment, container, false);
     }
+    private void intHeader() {
+        headerLayout = (HeaderLayout) getView().findViewById(R.id.headerLayout);
+        headerLayout.showTitle(App.ctx.getString(R.string.contact));
 
-    private void findView() {
-        View fragmentView = getView();
-        yueButton = (Button) fragmentView.findViewById(R.id.btn_yue);
-        yueButton.setOnClickListener(this);
+        headerLayout.showRightImageButton(R.drawable.base_action_bar_filter, new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.goActivity(ctx, YueUpdateActivity.class);
+            }
+
+        });
+        headerLayout.showRightImageButton(R.drawable.base_action_bar_add_bg_selector, new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.goActivity(ctx, YueUpdateActivity.class);
+            }
+
+        });
+
     }
+//    private void findView() {
+//        View fragmentView = getView();
+//        yueButton = (Button) fragmentView.findViewById(R.id.btn_yue);
+//        yueButton.setOnClickListener(this);
+//    }
 
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.btn_yue) {
-            Utils.goActivity(ctx, YueUpdateActivity.class);
-
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        int id = v.getId();
+//        if (id == R.id.btn_yue) {
+//            Utils.goActivity(ctx, YueUpdateActivity.class);
+//
+//        }
+//    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -87,17 +109,18 @@ public class DiscoverFragment extends BaseFragment implements View.OnClickListen
         commentEdit = (EditText) getView().findViewById(R.id.comment_text_edit);
         commentBtn = (Button) getView().findViewById(R.id.commentBtn);
         headerLayout.showTitle(R.string.discover);
-        headerLayout.showRightImageButton(R.drawable.nearby_order, new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle(R.string.sort).setPositiveButton(R.string.loginTime,
-                        updatedAtListener).setNegativeButton(R.string.distance, distanceListener).show();
-            }
-        });
+//        headerLayout.showRightImageButton(R.drawable.nearby_order, new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//                builder.setTitle(R.string.sort).setPositiveButton(R.string.loginTime,
+//                        updatedAtListener).setNegativeButton(R.string.distance, distanceListener).show();
+//            }
+//        });
+        intHeader();
         initXListView();
         listView.onRefresh();
-        findView();
+//        findView();
     }
 
 
